dd97719f163817f63fab853c595b12d81a282af6
piupuer <piupuer@gmail.com>
2023-04-19 16:47:46 +0800

    Revert "version"
    This reverts commit 28b6d570eece88bb1164a8ac3272d8f381a3f638.

--- cmd/cinch/version.go
+++ cmd/cinch/version.go
@@ -1,15 +1,3 @@
 package main
 
-import "runtime/debug"
-
-var release = func() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				return setting.Value
-			}
-		}
-	}
-
-	return "dev"
-}()
+const release = "v1.0.0"
