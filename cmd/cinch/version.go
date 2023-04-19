28b6d570eece88bb1164a8ac3272d8f381a3f638
piupuer <piupuer@gmail.com>
2023-04-19 16:35:56 +0800

    version

--- cmd/cinch/version.go
+++ cmd/cinch/version.go
@@ -1,3 +1,15 @@
 package main
 
-const release = "v1.0.0"
+import "runtime/debug"
+
+var release = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+
+	return "dev"
+}()
